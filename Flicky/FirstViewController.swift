501a679f04c71130e2e48b57e660f5e6a10ce3a6
Kenny Orellana <kenny.orellana504@gmail.com>
2020-09-15 17:35:36 -0600

    Initial Commit

---
+++ Flicky/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Flicky
+//
+//  Created by Kenny Orellana on 9/15/20.
+//  Copyright © 2020 Kenny Orellana. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flicky/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Flicky
+//
+//  Created by Kenny Orellana on 9/15/20.
+//  Copyright © 2020 Kenny Orellana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
